36cca0fd4e940accca9611bc5fd0b475bfd70c33
C Ewen Mac MILLAN <youenmacmillan@gmail.com>
2023-08-18 14:35:52 +0200

    Correct Prefixing of trait names

--- Source/MindMattersTraits.cs
+++ Source/MindMattersTraits.cs
@@ -5,24 +5,24 @@ namespace MindMatters
 {
     public static class MindMattersTraits
     {
-        public static readonly TraitDef Adventurous = TraitDef.Named("Adventurous");
-        public static readonly TraitDef Cautious = TraitDef.Named("Cautious");
-        public static readonly TraitDef Desensitized = TraitDef.Named("Desensitized");
-        public static readonly TraitDef OpenMinded = TraitDef.Named("OpenMinded");
-        public static readonly TraitDef Organized = TraitDef.Named("Organized");
-        public static readonly TraitDef Outgoing = TraitDef.Named("Outgoing");
-        public static readonly TraitDef Pluviophile = TraitDef.Named("Pluviophile");
-        public static readonly TraitDef Prude = TraitDef.Named("Prude");
-        public static readonly TraitDef Relaxed = TraitDef.Named("Relaxed");
-        public static readonly TraitDef Reserved = TraitDef.Named("Reserved");
-        public static readonly TraitDef TenderHearted = TraitDef.Named("TenderHearted");
-        public static readonly TraitDef SelfCentered = TraitDef.Named("SelfCentered");
-        public static readonly TraitDef Socialite = TraitDef.Named("Socialite");
-        public static readonly TraitDef Bipolar = TraitDef.Named("Bipolar");
-        public static readonly TraitDef Empathetic = TraitDef.Named("Empathetic");
-        public static readonly TraitDef Unstable = TraitDef.Named("Unstable");
-        public static readonly TraitDef Paranoid = TraitDef.Named("Paranoid");
-        public static readonly TraitDef Recluse = TraitDef.Named("Recluse");
+        public static readonly TraitDef Adventurous = TraitDef.Named("MM_Adventurous");
+        public static readonly TraitDef Cautious = TraitDef.Named("MM_Cautious");
+        public static readonly TraitDef Desensitized = TraitDef.Named("MM_Desensitized");
+        public static readonly TraitDef OpenMinded = TraitDef.Named("MM_OpenMinded");
+        public static readonly TraitDef Organized = TraitDef.Named("MM_Organized");
+        public static readonly TraitDef Outgoing = TraitDef.Named("MM_Outgoing");
+        public static readonly TraitDef Pluviophile = TraitDef.Named("MM_Pluviophile");
+        public static readonly TraitDef Prude = TraitDef.Named("MM_Prude");
+        public static readonly TraitDef Relaxed = TraitDef.Named("MM_Relaxed");
+        public static readonly TraitDef Reserved = TraitDef.Named("MM_Reserved");
+        public static readonly TraitDef TenderHearted = TraitDef.Named("MM_TenderHearted");
+        public static readonly TraitDef SelfCentered = TraitDef.Named("MM_SelfCentered");
+        public static readonly TraitDef Socialite = TraitDef.Named("MM_Socialite");
+        public static readonly TraitDef Bipolar = TraitDef.Named("MM_Bipolar");
+        public static readonly TraitDef Empathetic = TraitDef.Named("MM_Empathetic");
+        public static readonly TraitDef Unstable = TraitDef.Named("MM_Unstable");
+        public static readonly TraitDef Paranoid = TraitDef.Named("MM_Paranoid");
+        public static readonly TraitDef Recluse = TraitDef.Named("MM_Recluse");
         public static readonly TraitDef Nerves = TraitDef.Named("Nerves");
         public static readonly TraitDef Abrasive = TraitDef.Named("Abrasive");
         public static readonly TraitDef Ascetic = TraitDef.Named("Ascetic");
